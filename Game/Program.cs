c64adce92e512f0ce6e5967793463d3df39e4dd1
Carlie-Dev <ellepheonixgames@gmail.com>
2024-08-25 12:45:21 -0500

    Add project files.

---
+++ Game/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Game
+{
+    internal class Program
+    {
+        Player player1 = new Player();
+        Player player2 = new Player();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Rumble Dice! \nGet ready to face your aponent!");
+
+            
+        }
+
+    }
+
+    class Player
+    {
+        private string name { get; set; }
+        private int health { get; set; }
+
+        private Weapon weapon { get; set; }
+
+        public Player(string name, int health, string weapon)
+        {
+            this.name = name;
+            this.health = health;
+            this.weapon = weapon;
+        }
+        
+    }
+    class Weapon
+    {
+        string weaponType = "";
+        string weaponAction = "";
+    }
+
+  
+}
